dc2d64c37b733d926cf7727012c3c2f887da3dbc
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-06-12 10:24:49 +0900

    [level 1] Title: 시저 암호, Time: 1.55 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,19 +1,17 @@
 def solution(s, n):
-    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
-    upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     result = []
-
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
     for char in s:
-        if char in lower_alphabet:
-            # 소문자인 경우
-            index = lower_alphabet.index(char)
-            shifted_index = (index + n) % 26
-            result.append(lower_alphabet[shifted_index])
-        elif char in upper_alphabet:
-            # 대문자인 경우
-            index = upper_alphabet.index(char)
-            shifted_index = (index + n) % 26
-            result.append(upper_alphabet[shifted_index])
+        if char in lower:
+            num = lower.index(char)
+            shift = lower[(num+n)%26]
+            result.append(shift)
+        elif char in upper:
+            num = upper.index(char)
+            shift = upper[(num+n)%26]
+            result.append(shift)
         else:
             # 알파벳이 아닌 경우 (공백 등)
             result.append(char)
