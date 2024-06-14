007bcbd4637f6d33dc1f692f129b6b87f6c055dd
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-06-14 11:25:42 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.91 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    word = {}
+    for i in range(len(s)):
+        if s[i] not in word:
+            word[s[i]] = i
+            answer.append(-1)
+        else:
+            answer.append(i-word[s[i]])
+            word[s[i]] = i
+    
+    return answer
