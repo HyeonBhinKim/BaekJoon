a01b9d088694e58f123aecfb7d07adc913e3470d
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-06-26 17:59:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,8 +2,7 @@ def solution(k, score):
     answer = []
     result = []
     for i in score:
-        if len(answer) <= k:
-            answer.append(i)
+        answer.append(i)
         if len(answer) > k:
             answer.remove(min(answer))
             
