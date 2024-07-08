a93340fe0f642d90a58239ea9b7135005b474e7b
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-07-08 19:50:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,17 +1,26 @@
+# def solution(babbling):
+#     answer = 0
+#     cansay = ["aya", "ye", "woo", "ma"]
+#     for i in babbling:
+#         tmp = ''
+#         said = ''
+#         for j in i:
+#             tmp += j
+#             if tmp in cansay and tmp != said:
+#                 said = tmp
+#                 tmp = ''
+#             if len(tmp) > 3:
+#                 break
+#         if tmp == '':
+#             answer +=1
+        
+#     return answer
 def solution(babbling):
     answer = 0
-    cansay = ["aya", "ye", "woo", "ma"]
     for i in babbling:
-        tmp = ''
-        said = ''
-        for j in i:
-            tmp += j
-            if tmp in cansay and tmp != said:
-                said = tmp
-                tmp = ''
-            if len(tmp) > 3:
-                break
-        if tmp == '':
+        for j in ['aya','ye','woo','ma']:
+            if j*2 not in i:
+                i=i.replace(j,' ')
+        if len(i.strip())==0:
             answer +=1
-        
     return answer
