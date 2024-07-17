8dee5b19713ae591984a4bf633019efcb29b5d6a
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-07-17 17:07:34 +0900

    [level 1] Title: 햄버거 만들기, Time: 259.83 ms, Memory: 26.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,18 @@
+def solution(ingredient):
+    
+    tmp = []
+    answer = 0
+    
+    for i in ingredient:
+        tmp.append(i) # stack 시간복잡도 O(1)
+        if len(tmp) >= 4 and tmp[-4:] == [1,2,3,1]:
+            del tmp[-4:]
+            answer += 1
+    
+    # for i in ingredient:
+    #     tmp += str(i)  시간복잡도 O(n)
+    #     if '1231' in tmp:  시간복잡도 O(n) => O(n^2)
+    #         answer += 1
+    #         tmp = tmp.replace('1231','')  시간복잡도 O(n)
+        
+    return answer
