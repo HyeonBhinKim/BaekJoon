4846e5066273e4fb3bbf70c46e73b572906bee4d
HyeonBhinKim <122499657+HyeonBhinKim@users.noreply.github.com>
2024-07-17 16:29:01 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,17 +1,16 @@
 def solution(s, skip, index):
-    # 알파벳 리스트에서 skip에 포함된 문자를 제외
-    alphabets = [chr(i) for i in range(97, 123) if chr(i) not in skip]
     
-    # 변환된 결과를 저장할 리스트
-    result = []
+    alphabet = []
+    answer = []
     
-    for char in s:
-        # 현재 문자의 위치를 찾음
-        current_pos = alphabets.index(char)
-        # index만큼 뒤의 위치를 계산
-        new_pos = (current_pos + index) % len(alphabets)
-        # 변환된 문자를 결과에 추가
-        result.append(alphabets[new_pos])
+    for i in range(97, 123):
+        if chr(i) not in skip:
+            alphabet.append(chr(i))
     
-    # 리스트를 문자열로 변환하여 반환
-    return ''.join(result)
+    for spel in s:
+        tmp = alphabet.index(spel)
+        new_spel = alphabet[(tmp+index)%len(alphabet)]
+        answer.append(new_spel)
+    
+    return ''.join(answer)
+    
